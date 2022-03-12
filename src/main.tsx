7b9b377c25f28abd5db75158a86b74a81ecbfb5f
Max Sosnov <crazymax101@gmail.com>
2022-03-12 21:05:17 +0700

    Регистрирует сервис-воркер

--- src/main.tsx
+++ src/main.tsx
@@ -2,8 +2,14 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 
+// @ts-ignore
+import { registerSW } from 'virtual:pwa-register';
 import { App } from './App';
 
+registerSW({
+  onNeedRefresh() {},
+  onOfflineReady() {},
+});
 ReactDOM.render(
   <React.StrictMode>
     <App />
